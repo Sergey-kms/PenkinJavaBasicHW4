64df3ac04c8aa4f563e55bfd8727905f32e34b11
Sergey-kms <penkinsergeykms@gmail.com>
2023-11-22 09:29:19 +0800

    Commit1 vetka1

--- src/main/java/ru/penkin/java/basic/hw4/Box.java
+++ src/main/java/ru/penkin/java/basic/hw4/Box.java
@@ -1,2 +1,85 @@
-package ru.penkin.java.basic.hw4;public class Box {
+package ru.penkin.java.basic.hw4;
+
+public class Box {             // На гитхаб vetka1
+    private final int width;
+    private final int height;
+    private final int depth;
+    private String color;
+    private boolean open;
+    private String object;
+
+    public Box(int width, int height, int depth, String color) {        // Создание коробки
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+        this.color = color;
+        this.open = false;
+        this.object = null;
+    }
+
+    public void openBox() {              // Открытие коробки
+        if (!open) {
+            open = true;
+            System.out.println("Коробка открыта");
+        } else {
+            System.out.println("Коробка уже открыта");
+        }
+    }
+
+    public void closeBox() {             // Закрытие коробки
+
+        if (open) {
+            open = false;
+            System.out.println("Коробка закрыта");
+        } else {
+            System.out.println("Коробка уже закрыта");
+        }
+    }
+
+    public void repaint(String newColor) {      // Перекрашивание коробки
+        color = newColor;
+        System.out.println("Коробка перекрашена в цвет: " + color);
+    }
+
+    public void printInfo() {            // Вывод информации о коробке
+        System.out.println("Размеры коробки: " + width + "x" + height + "x" + depth);
+        System.out.println("Цвет коробки: " + color);
+        System.out.println("Состояние коробки: " + (open ? "открыта" : "закрыта"));
+        System.out.println("Предмет в коробке: " + (object != null ? object : "нет предмета"));
+    }
+
+    public void addAnObject(String newObject) {    // Добавление предмета в коробку
+        if (open && object == null) {
+            object = newObject;
+            System.out.println("Предмет добавлен в коробку: " + object);
+        } else if (!open) {
+            System.out.println("Необходимо открыть коробку перед добавлением .");
+        } else {
+            System.out.println("В коробке уже есть предмет. Выкините его, чтобы добавить новый");
+        }
+    }
+
+    public void layOutObject() {                // Выкладывание предмета из коробки
+        if (open && object != null) {
+            object = null;
+            System.out.println("Предмет выложен из коробки");
+        } else if (!open) {
+            System.out.println("Необходимо открыть коробку перед выкладыванием предмета");
+        } else {
+            System.out.println("В коробке нет предмета для выкладывания");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Box Box = new Box(100, 60, 60, "желтый");
+        Box.printInfo();
+        Box.openBox();
+        Box.addAnObject("Ноутбук");
+        Box.printInfo();
+        Box.closeBox();
+        Box.repaint("Красный");
+        Box.layOutObject();
+        Box.printInfo();
+    }
 }
--- src/main/java/ru/penkin/java/basic/hw4/User.java
+++ src/main/java/ru/penkin/java/basic/hw4/User.java
@@ -1,2 +1,49 @@
-package ru.penkin.java.basic.hw4;public class User {
+package ru.penkin.java.basic.hw4;
+
+public class User {          // На гитхаб vetka1
+    private String name;
+    private String surname;
+    private String middlename;
+    private int yearbirth;
+    private String email;
+
+    public User(String lastName, String firstName, String middleName, int birthYear, String email) {
+        this.name = lastName;
+        this.surname = firstName;
+        this.middlename = middleName;
+        this.yearbirth = birthYear;
+        this.email = email;
+    }
+
+    public void printInfo() {
+        System.out.println("ФИО: " + name + " " + surname + " " + middlename);
+        System.out.println("Год рождения: " + yearbirth);
+        System.out.println("e-mail: " + email);
+    }
+
+    public int getAge() {
+        return 2023 - yearbirth;
+    }
+
+    public static void main(String[] args) {
+        User[] users = new User[10];
+
+        users[0] = new User("Березов", "Роман", "Сергеевич", 1979, "berezov@mail.com");
+        users[1] = new User("Листов", "Кузьма", "Александрович", 1990, "listov@mail.com");
+        users[2] = new User("Грибов", "Андрей", "Александрович", 1988, "petrenko@mail.com");
+        users[3] = new User("Сидоров", "Сидр", "Александрович", 1975, "sidorov@mail.com");
+        users[4] = new User("Козлов", "Виктор", "Александрович", 1995, "kozlov@mail.com");
+        users[5] = new User("Цуциков", "Антон", "Алексеевич", 1972, "tsutsikov@mail.com");
+        users[6] = new User("Мамонтов", "Иван", "Кузьмич", 1957, "mamonтov@maile.com");
+        users[7] = new User("Кузьмин", "Владислав", "Петрович", 1976, "kuzmin@mail.com");
+        users[8] = new User("Никаноров", "Станислав", "Кузьма", 1991, "nikanorov@mail.com");
+        users[9] = new User("Карасев", "Вячеслав", "Петрович", 1960, "karasev@mail.com");
+
+        for (User user : users) {
+            if (user.getAge() > 40) {
+                user.printInfo();
+                System.out.println();
+            }
+        }
+    }
 }
